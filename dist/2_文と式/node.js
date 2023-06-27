b0512495b82f4a14ae9c919f4c14187687cdccd0
kazuki.watanabe <kazuki.watanabe@revcomm.co.jp>
2023-06-27 14:45:49 +0900

    update

---
+++ dist/2_文と式/node.js
@@ -0,0 +1,13 @@
+import { createInterface } from "readline";
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+rl.question("What is your name? ", (name) => {
+    console.log(`Hello ${name}`);
+    rl.close();
+});
+rl.question("What is your age? ", (age) => {
+    console.log(`You are ${age} years old`);
+    rl.close();
+});
